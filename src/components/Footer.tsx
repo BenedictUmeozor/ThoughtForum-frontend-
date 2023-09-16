7bb66187d9a510c4223118f45031141465ccc9a6
Benedict <benumeozor@gmail.com>
2023-09-16 23:55:17 +0100

    footer redesign

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,37 @@
 import { Link } from "react-router-dom";
 import {
+  EnvelopeIcon,
   FacebookIcon,
   InstagramIcon,
   LinkedinIcon,
+  PhoneIcon,
   TwitterIcon,
 } from "../assets/icons";
+import { useAppSelector } from "../hooks/hooks";
 
 const Footer = () => {
+  const categories = useAppSelector((state) => state.categories);
+
   return (
     <footer>
-      <div className="container">
-        <div className="flex">
-          <Link to="/" className="logo">
-            <h1>
-              Thought<span>Forum</span>
-            </h1>
+      <div className="container main-footer">
+        <div className="title">
+          <Link to="/">
+            Thought<span>Forum</span>
           </Link>
+          <p>Unlocking Answers, Igniting Questions</p>
+        </div>
+        <div className="footer-categories">
+          <h2>Categories</h2>
+          <ul>
+            {categories.map((category) => (
+              <li key={category._id}>
+                <Link to={"categories/" + category._id}>{category.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="contact">
           <div className="socials">
             <Link to="/">
               <FacebookIcon className="icon" />
@@ -30,8 +46,18 @@ const Footer = () => {
               <LinkedinIcon className="icon" />
             </Link>
           </div>
+          <p>
+            <PhoneIcon className="icon" />
+            <span>08108218964</span>
+          </p>
+          <p>
+            <EnvelopeIcon className="icon" />
+            <a href="mailto:benedictumeozor@gmail.com">Send an email</a>
+          </p>
         </div>
-        <p className="copyright">&copy; 2023 Benedict</p>
+      </div>
+      <div className="copyright container">
+        <p>All rights reserved | Benedict &copy; 2023</p>
       </div>
     </footer>
   );
